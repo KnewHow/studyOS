14d4040fd824387c9a8226529c35d2bbd42925b2
yghjava <948170910@qq.com>
2017-05-05 08:07:25 +0800

    third commit

--- ygh-test/src/main/java/ygh/test/Demo2.java
+++ ygh-test/src/main/java/ygh/test/Demo2.java
@@ -1,12 +1,8 @@
 package ygh.test;
 
-public class Demo1 {
+public class Demo2 {
 
 	public static void main(String[] args) {
 		System.out.println("just test");
-	}
-	
-	public void fun2(){
-		System.out.println("fun1");
 	}
 }
